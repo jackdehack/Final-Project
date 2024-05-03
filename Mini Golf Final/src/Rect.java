90e7d5f1aabe9fa2fbdb7375fdcd63ace7752775
Jack W <jww05@icloud.com>
2024-05-02 21:33:48 -0400

    new class

---
+++ Mini Golf Final/src/Rect.java
@@ -0,0 +1,16 @@
+
+public class Rect extends CollisionObj{
+	private int width;
+	private int height;
+
+	public Rect(double x, double y) {
+		super(x, y);
+	}
+	
+	
+	
+	
+	
+	
+
+}
--- Mini Golf Final/src/Wall.java
+++ Mini Golf Final/src/Wall.java
@@ -1,9 +1,18 @@
 
 public class Wall extends CollisionObj{
+	private int rotation;
 
 	public Wall(double x, double y) {
 		super(x, y);
 	
+	}
+
+	public int getRotation() {
+		return rotation;
+	}
+
+	public void setRotation(int rotation) {
+		this.rotation = rotation;
 	}
 
 }
