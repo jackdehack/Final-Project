fbcbc32e79b4716a1d01e83a41e047dadddc4d96
OwensOwen <liamgelbinbergin@gmail.com>
2024-05-10 12:15:04 -0400

    added draw

--- Mini Golf Final/src/Wall.java
+++ Mini Golf Final/src/Wall.java
@@ -1,9 +1,20 @@
+import java.awt.Graphics;
 
-public class Wall extends CollisionObj{
+public class Wall extends CollisionObj {
 	private int rotation;
+	private int pointx;
+	private int pointy;
+	private int width;
+	private int height;
 
-	public Wall(double x, double y) {
+	public Wall(int x, int y, int width, int height,Graphics g) {
+		
 		super(x, y);
+		pointx = x;
+		pointy = y;
+		Draw obstacle = new Draw(x,y,g,width,height);
+		
+		
 	
 	}
 
