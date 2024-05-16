36fe603aa427b1f5a3d5cdee2c3d118f3b0b6952
OwensOwen <liamgelbinbergin@gmail.com>
2024-05-16 12:15:48 -0400

    pusg
    push

--- Mini Golf Final/src/Draw.java
+++ Mini Golf Final/src/Draw.java
@@ -12,8 +12,20 @@ public class Draw extends JPanel{
 	
 	//jhkhkhjkh
 	public void paintComponent(Graphics g) {
-		Wall leftWall = new Wall(10, 10, 100, 4, g);
+		Wall leftWall = new Wall(250, 10, 4, 100, g);
+		Wall tWall = new Wall(250, 10, 100, 4, g);
+		Wall rWall = new Wall(350, 10, 4, 400, g);
+		Wall bWall = new Wall(250, 110, 50, 4, g);
+		Wall sWall = new Wall(300, 110, 4, 200, g);
+		Wall btWall = new Wall(250, 310, 50, 4, g);
+		Wall blWall = new Wall(250, 310, 4, 100, g);
+		Wall bbWall = new Wall(250, 410, 100, 4, g);
 		leftWall.drawWall(g);
+		tWall.drawWall(g);
+		rWall.drawWall(g);
+		bWall.drawWall(g);
+		btWall.drawWall(g);
+		
 	}
 	
 	
--- Mini Golf Final/src/Driver.java
+++ Mini Golf Final/src/Driver.java
@@ -8,8 +8,9 @@ public class Driver {
 		frame.setTitle("Minigolf");
 		frame.setSize(1200, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Draw d = new Draw(400, 400);
-        frame.add(d);
+        Draw topWall = new Draw(400, 400);
+       
+        frame.add(topWall);
         frame.setVisible(true);
         
     }
