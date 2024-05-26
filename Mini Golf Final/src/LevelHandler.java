86b696db53ce84e4a1cb0efeae1424d16be14cea
jackdehack <45639748+jackdehack@users.noreply.github.com>
2024-05-26 19:02:32 -0400

    Revert "Merge branch 'main' of https://github.com/jackdehack/Mini-Golf-Final-Project"
    This reverts commit 5725ceaf9ab76d080cb34484e2bf1b85ce116ee7, reversing
    changes made to 4d6f8efc58b77ac69e02079ad70747e67437abb2.

--- Mini Golf Final/src/LevelHandler.java
+++ Mini Golf Final/src/LevelHandler.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+
 import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -16,9 +17,18 @@ public class LevelHandler extends JPanel {
     Hole h;
     Point initialClick;
     ArrayList<Point> projectionPoints;
-    double maxSpeed = 10.0; // Define maximum speed for the ball
+    static int[] holeSwings = new int[5];    
+    int swings;
+    int par;
 
-    public LevelHandler(int width, int height) {
+    
+    
+    double maxSpeed = 6.5; 
+
+    
+    
+    
+    public LevelHandler(int width, int height, int level) {
         try {
             bg = ImageIO.read(new File("Background.png"));
         } catch (IOException e) {
@@ -28,12 +38,35 @@ public class LevelHandler extends JPanel {
         this.width = width;
         this.height = height;
         projectionPoints = new ArrayList<>();
-        levelOne();
+        
+        
+        if(level == 1) {
+        	levelOne();
+        }
+        
+        if(level == 2) {
+            levelTwo();
+        }
+        
+        if(level == 3) {
+            levelThree();
+        }
+        
+        if(level == 4) {
+            levelFour();
+        }
+        
+        if(level == 5) {
+            levelFive();
+        }
+        
+        
+       
 
         addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
-                if (b.contains(e.getPoint())) {
+                if (b.contains(e.getPoint()) && b.ballv.magnitude() == 0) {
                     initialClick = e.getPoint();
                 }
             }
@@ -49,6 +82,7 @@ public class LevelHandler extends JPanel {
                     b.ballv = capSpeed(newVelocity, maxSpeed);
                     initialClick = null;
                     projectionPoints.clear();
+                    swings++;
                 }
             }
         });
@@ -68,6 +102,12 @@ public class LevelHandler extends JPanel {
             }
         });
     }
+    
+    
+    
+    
+    
+    
 
     private Vector capSpeed(Vector velocity, double maxSpeed) {
         double speed = velocity.magnitude();
@@ -80,37 +120,136 @@ public class LevelHandler extends JPanel {
 
     private void updateProjectionPoints(Vector velocity) {
         projectionPoints.clear();
-        double timeStep = 0.1;
-        double dampingFactor = 0.995; // Less aggressive damping for a longer projection
+        double timeStep = 4;
         double x = b.x;
         double y = b.y;
         double vx = velocity.x;
         double vy = velocity.y;
 
-        for (int i = 0; i < 100; i++) { // Further increased number of points for longer projection
+        for (int i = 0; i < velocity.magnitude() * 1.5; i++) {
             x += vx * timeStep;
             y += vy * timeStep;
             projectionPoints.add(new Point((int) x, (int) y));
-            vx *= dampingFactor;
-            vy *= dampingFactor;
         }
     }
 
     public void levelOne() {
+    	par = 2;
+    	
         b = new Ball(340, 690, new Vector(0, 0));
         h = new Hole(340, 90);
 
+        
         Wall lWall = new Wall(230, 35, 760, 90);
         Wall rWall = new Wall(455, 35, 760, 90);
         Wall tWall = new Wall(230, 35, 221, 0);
         Wall bWall = new Wall(230, 791, 221, 0);
 
+        
         walls = new ArrayList<Wall>();
         walls.add(lWall);
         walls.add(rWall);
         walls.add(tWall);
         walls.add(bWall);
     }
+    
+    
+    
+    public void levelTwo() {
+    	par = 3;
+    	
+    	
+    	b = new Ball(240, 670, new Vector(0, 0));
+        h = new Hole(230, 90);
+        
+        
+    	Wall wall1 = new Wall(190, 35, 200, 90);
+    	Wall wall2 = new Wall(190, 35, 100, 0);
+    	Wall wall3 = new Wall(290, 35, 150, 45);
+    	Wall obstacle = new Wall(320, 180, 40, 40, -15);
+    	Wall wall4 = new Wall(186, 235, 104, 0);
+    	Wall wall5 = new Wall(396, 141, 450, 90);
+    	Wall wall6 = new Wall(290, 235, 250, 90);
+    	Wall wall7 = new Wall(290, 485, 150, 135);
+    	Wall wall8 = new Wall(396, 591, 106, 180);
+    	Wall wall9 = new Wall(186, 587, 100, 90);
+    	Wall wall10 = new Wall(182, 687, 110, 0);
+    	Wall wall11 = new Wall(294, 591, 100, 90);
+    	
+    	
+    	walls = new ArrayList<Wall>();
+    	
+    	
+    	walls.add(wall1);
+    	walls.add(wall2);
+    	walls.add(wall3);
+    	walls.add(wall4);
+    	walls.add(wall5);
+    	walls.add(wall6);
+    	walls.add(wall7);
+    	walls.add(wall8);
+    	walls.add(wall9);
+    	walls.add(wall10);
+    	walls.add(wall11);
+    	walls.add(obstacle);  
+    	
+    }
+    
+    
+    public void levelThree() {
+    	par = 4;
+    	
+    	
+    	b = new Ball(440, 650, new Vector(0, 0));
+        h = new Hole(320, 140);
+        
+        
+    	Wall wall1 = new Wall(380, 700, 130, 0);
+    	Wall wall2 = new Wall(380, 600, 130, 0);
+    	Wall wall3 = new Wall(510, 600, 104, 90);
+    	Wall obstacle = new Wall(300, 220, 50, 50, 15);
+    	Wall obstacle2 = new Wall(290, 420, 10, 60, -15);
+    	Wall obstacle3 = new Wall(270, 620, 10, 65, -45);
+    	Wall wall4 = new Wall(380, 700, 100, 90);
+    	Wall wall5 = new Wall(380, 800, 120, 180);
+    	Wall wall6 = new Wall(380, 100, 120, 180);
+    	Wall wall7 = new Wall(380, 600, 504, -90);
+    	Wall wall8 = new Wall(260, 96, 704, 90);
+    	
+    	
+    	walls = new ArrayList<Wall>();
+    	
+    	
+    	walls.add(wall1);
+    	walls.add(wall2);
+    	walls.add(wall3);
+    	walls.add(wall4);
+    	walls.add(wall5);
+    	walls.add(wall6);
+    	walls.add(wall7);
+    	walls.add(wall8);
+    	walls.add(obstacle); 
+        walls.add(obstacle2);  
+        walls.add(obstacle3);  
+    }
+    
+    
+    public void levelFour() {
+    	
+    }
+    
+    
+    public void levelFive() {
+    	
+    }
+    
+    
+    
+    
+    
+    
+    
+    
 
     public void paintComponent(Graphics k) {
         super.paintComponent(k); // Call the superclass method to ensure the panel is properly rendered
@@ -119,20 +258,26 @@ public class LevelHandler extends JPanel {
         for (Wall w : walls) {
             w.drawWall(g);
         }
-        b.draw(g);
         h.draw(g);
+        b.draw(g);
 
         if (initialClick != null) {
             drawProjectionLine(g);
         }
     }
+    
+    
+    
+    
 
     private void drawProjectionLine(Graphics2D g) {
-        g.setColor(Color.RED);
+        g.setColor(Color.white);
         for (int i = 0; i < projectionPoints.size(); i++) {
             Point p = projectionPoints.get(i);
-            int size = Math.max(2, 10 - i / 10); // Dots get smaller further from the ball
-            g.fillOval(p.x - size / 2, p.y - size / 2, size, size);
+            g.fillOval(p.x, p.y, 4, 4);
         }
     }
+    
+    
+    
 }
